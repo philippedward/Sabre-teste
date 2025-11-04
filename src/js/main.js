4971629d2870d5d45b94495273eb0e390200ab3f
philippedward <amelie.rakowiecki@studentfr.ch>
2025-11-04 15:54:07 +0100

    recommencer

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const circle = document.getElementById("space-circle");
 const galaxie = document.querySelector(".space-rectangle-galaxie");
 const blackHole = document.querySelector(".space-rectangle-black-hole");
 const circleBlackHole = document.getElementById("space-circle-black-hole");
+const spaceClicking = document.getElementById("space-clicking");
+const cloud1 = document.querySelector(".space-rectangle-cloud1");
+const cloud2 = document.querySelector(".space-rectangle-cloud2");
+const cloud3 = document.querySelector(".space-rectangle-cloud3");
+
+let scrollProgress = 0;
 
 circle.addEventListener("click", function () {
-  // Zoom rapide sur le cercle
   circle.style.transform = "scale(100)";
 
-  // Après l'animation, masquer la galaxie
   setTimeout(() => {
     galaxie.classList.add("hide");
     circleBlackHole.style.opacity = "1";
@@ -15,6 +24,12 @@ circle.addEventListener("click", function () {
 });
 
 circleBlackHole.addEventListener("click", function () {
-  // Lancer l'animation de dézoom
   blackHole.classList.add("dezoom");
+
+  setTimeout(() => {
+    spaceClicking.style.display = "none";
+    document
+      .querySelector(".scroll-blocked")
+      .classList.remove("scroll-blocked");
+  }, 2000);
 });
