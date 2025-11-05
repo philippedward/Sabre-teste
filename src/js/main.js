64e754064d69eb9700ac160784f117ef896c76ac
philippedward <amelie.rakowiecki@studentfr.ch>
2025-11-05 11:18:17 +0100

    space click et no scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,33 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+const galaxy = document.querySelector(".space-galaxy");
+const spaceContainer = document.querySelector(".container.space");
+const blackHole = document.querySelector("#space-blackHole");
+
+document.body.style.overflow = "hidden";
+
+blackHole.addEventListener("click", () => {
+  gsap.to(blackHole, {
+    scale: 30,
+    duration: 0.8,
+    ease: "power2.out",
+    onComplete: () => {
+      document.body.style.overflow = "auto";
+    },
+  });
+});
+
+const earthTimline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".earth-space-groupe",
+    start: "bottom bottom",
+    scrub: true,
+    pin: true,
+  },
+});
+
+earthTimline.to(".earth-cloud1", { opacity: 1 });
+earthTimline.to(".earth-cloud2", { opacity: 1 });
+earthTimline.to(".earth-cloud3", { opacity: 1 });
